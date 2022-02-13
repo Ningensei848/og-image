64f8a119f1160881acb4800d380eadde82abcf62
Ningensei848 <Ningensei848@users.noreply.github.com>
2022-02-13 01:40:34 +0000

    deploy: 928d6aabce0624832b12f70b9735ad42b87e7374

---
+++ _next/static/build-id/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4cb02b1f6f1f8fb1.js"],"/_error":["static/chunks/pages/_error-d742f979193aeae4.js"],"/_middleware":["server/middleware-runtime.js","server/pages/_middleware.js"],sortedPages:["/","/_app","/_error","/_middleware"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
